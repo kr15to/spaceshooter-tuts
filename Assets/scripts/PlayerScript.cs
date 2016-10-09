b1c9db179795d6ef2979f3b6aaa16cc3b21600a1
Krishna Wahyu <krishna.wahyu@gmail.com>
2016-10-09 15:18:49 +0700

    initial commit
    commit pertama, dengan beberapa tambahan asset awal dan script untuk
    gerakan pesawat

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(10, 10);
+	private Vector2 movement;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// movement per direction
+		movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		if(rb2d == null) {
+			rb2d = GetComponent<Rigidbody2D>();
+		}
+		// move the ship
+		rb2d.velocity = movement;
+	}
+}
---
+++ Assets/scripts/SimpleMoveScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMoveScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 direction = new Vector2(0, -1);
+	private Vector2 movement;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void FixedUpdate() {
+		if(rb2d == null) {
+			rb2d = GetComponent<Rigidbody2D>();
+		}
+		rb2d.velocity = movement;
+	}
+}
